a10eb2170e27dc79d24191f48b865fd719318dba
Kyuwoong Lee <kwooler@gmail.com>
2022-10-19 00:54:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import random 
 
-def bubble(numbers):
+
+def foldandswap(numbers):
 	#################
 	# Make your code 
 	#################
 
-	
-	
-numbers = [2,3,0,5,4]
+
+numbers = [2,3,0,5,4,1,6,9,8,7]
 print (numbers)
-bubble(numbers)
+foldandswap(numbers)
 print (numbers)
 
-numbers = [ random.randint(0, 10) for i in range(10) ]
+
+numbers = [ random.randint(0,10) for i in range(10)]
 print (numbers)
-bubble(numbers)
-pritn (numbers)
+foldandswap(numbers)
+print (numbers)
+
+
