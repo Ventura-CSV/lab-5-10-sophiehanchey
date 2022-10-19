887d9c04e5d9052dd9f645e41788fd50b71d49cd
Kyuwoong Lee <kwooler@gmail.com>
2022-10-19 00:51:20 -0700

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+
+[Link to Chap 5 Lab9](https://docs.google.com/presentation/d/1r3h2R9JwK9HK_U2Ia-zncL0BSjHV6Giu6ugNJ6yZpgc/edit#slide=id.g16b637da4c8_3_116)
+
+![Lab2](https://nimbus-screenshots.s3.amazonaws.com/s/eb3fa7d494b365e9e3a3638ca3e7086b.png)
+
+## Compete the "main.py"
+
+
---
+++ main.py
@@ -0,0 +1,18 @@
+import random 
+
+def bubble(numbers):
+	#################
+	# Make your code 
+	#################
+
+	
+	
+numbers = [2,3,0,5,4]
+print (numbers)
+bubble(numbers)
+print (numbers)
+
+numbers = [ random.randint(0, 10) for i in range(10) ]
+print (numbers)
+bubble(numbers)
+pritn (numbers)
