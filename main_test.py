0b526dd98e58812f993a18a24667cf4fba66c22c
Kyu Lee <kwooler@gmail.com>
2022-10-20 00:33:25 -0700

    Lab 10

--- main_test.py
+++ main_test.py
@@ -5,13 +5,9 @@ import main
 def test_main():
     numbers = [5, 4, 3, 2, 1]
 
-    others = numbers
-    for i in range(len(numbers)-1):
-        findmin(others)
-        firstval, *others = others
-        numbers[i] = firstval
-
-    print(numbers)
+    print('Test data', numbers)
+    main.foldandswap(numbers)
+    print('After calling foldandswap()', numbers)
     assert numbers[0] == 1
     assert numbers[1] == 2
     assert numbers[2] == 3
@@ -19,3 +15,15 @@ def test_main():
     assert numbers[4] == 5
 # assert v1 == -10, "Min value does not match"
 # assert v2 == 5, "Max value does not match"
+    numbers = [8, 7, 6, 5, 4, 3, 2, 1]
+    print('Test data', numbers)
+    main.foldandswap(numbers)
+    print('After calling foldandswap()', numbers)
+    assert numbers[0] == 1
+    assert numbers[1] == 2
+    assert numbers[2] == 3
+    assert numbers[3] == 4
+    assert numbers[4] == 5
+    assert numbers[5] == 6
+    assert numbers[6] == 7
+    assert numbers[7] == 8
