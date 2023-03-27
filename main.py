d563394a2ef7da9173042bfb351e60a6cd6f89e4
Kyu Lee <kwooler@gmail.com>
2023-03-27 01:03:04 -0700

    L510

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-import random 
+import random
 
 
 def foldandswap(numbers):
-	#################
-	# Make your code 
-	#################
+    #################
+    # Make your code
+    #################
 
 
-numbers = [2,3,0,5,4,1,6,9,8,7]
-print (numbers)
-foldandswap(numbers)
-print (numbers)
+def main():
+    numbers = [2, 3, 0, 5, 4, 1, 6, 9, 8, 7]
+    print(numbers)
+    foldandswap(numbers)
+    print(numbers)
+
+    numbers = [random.randint(0, 10) for i in range(10)]
+    print(numbers)
+    foldandswap(numbers)
+    print(numbers)
 
 
-numbers = [ random.randint(0,10) for i in range(10)]
-print (numbers)
-foldandswap(numbers)
-print (numbers)
-
-
+if __name__ == '__main__':
+    main()
