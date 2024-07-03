107617f683bfa2afb7a4d33a9db4218cef52e7d7
Sophie Hanchey <sophiehanchey@gmail.com>
2024-07-02 17:19:26 -0700

    missing first number in swap

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 
 
 def foldandswap(numbers):
-    for i in (range(numbers/2)):
+    for i in range(int(len(numbers)/2)):
         sub = numbers[i]
         numbers[i] = numbers[-i]
         numbers[-i] = sub
