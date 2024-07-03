99565860609204a8362b80525ca5246d322e7795
Sophie Hanchey <sophiehanchey@gmail.com>
2024-07-02 17:25:22 -0700

    fixed right-side idx bug

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import random
 
 
 def foldandswap(numbers):
-    for i in range(0,int(len(numbers)/2)):
+    for i in range(int(len(numbers)/2)):
         sub = numbers[i]
-        numbers[i] = numbers[-i]
-        numbers[-i] = sub
+        numbers[i] = numbers[-(i+1)]
+        numbers[-(i+1)] = sub
 
 
 def main():
